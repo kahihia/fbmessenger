639b7bf955b9bcc1f5a2dca9b63bda5ee7729e6c
FZX <FZX@users.noreply.github.com>
2018-02-17 00:51:45 +0400

    Created models

--- fbautomation/userarea/models.py
+++ fbautomation/userarea/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+from django.contrib.auth.models import User
+import datetime
+
+
+class ProfileUrl(models.Model):
+
+    user = models.ForeignKey(User, blank=True, null=True,
+                             on_delete=models.SET_NULL)
+    url = models.CharField(max_length=500, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+    created_on = models.DateTimeField(default=datetime.datetime.now,
+                                      null=True, blank=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return "{}".format(self.url)
+
+
+class FacebookAccount(models.Model):
+
+    user = models.ForeignKey(User, blank=True, null=True,
+                             on_delete=models.SET_NULL)
+
+    username = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+
+    is_deleted = models.BooleanField(default=False)
+    created_on = models.DateTimeField(default=datetime.datetime.now,
+                                      null=True, blank=True)
+
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return "{}".format(self.username)
+
+
+class Message(models.Model):
+
+    user = models.ForeignKey(User, blank=True, null=True,
+                             on_delete=models.SET_NULL)
+
+    body = models.TextField(max_length=1000, null=True, blank=True)
+
+    is_deleted = models.BooleanField(default=False)
+    created_on = models.DateTimeField(default=datetime.datetime.now,
+                                      null=True, blank=True)

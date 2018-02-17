b64ea44dc2deb2e603d194243c77dc951fbc5010
FZX <FZX@users.noreply.github.com>
2018-02-17 21:37:50 +0400

    User avatar models

--- fbautomation/userarea/views.py
+++ fbautomation/userarea/views.py
@@ -27,3 +27,33 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+def profile(request):
+    if request.method == "POST":
+        password_form = PasswordChangeForm(request.user, request.POST)
+        user_form = UserForm(request.POST, instance=request.user)
+        image_form = UserImageForm(request.POST, instance=request.user)
+        if password_form.is_valid():
+            user = password_form.save()
+            update_session_auth_hash(request, user)
+        if user_form.is_valid():
+            user_form.save()
+
+        if image_form.is_valid():
+            user_image = UserImage.objects.filter(user=request.user)[0]
+            user_image.image = image_form.cleaned_data["image"]
+            user_image.save()
+        return redirect("profile")
+    else:
+        image_form = UserImageForm()
+        password_form = PasswordChangeForm(request.user)
+        user_form = UserForm(initial={
+            "first_name": request.user.first_name,
+            "last_name": request.user.last_name,
+            "email": request.user.email,
+            "username": request.user.username
+        })
+
+    return render(request, "profile.html", {"password_form": password_form,
+                                            "user_form": user_form,
+                                            "image_form": image_form})

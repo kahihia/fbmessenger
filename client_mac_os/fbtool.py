e4e0ef76323979db3f8dc474022eeaa3c97f538b
silverstar <silverstart1987913@yahoo.com>
2018-04-02 12:47:15 -0700

    add macos build code

---
+++ client_mac_os/fbtool.py
@@ -0,0 +1,217 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import os, sys
+
+DRIVER_PATH = "chromedriver"
+
+class Messenger():
+
+    def __init__(self, username, password, message, proxy=None, delay_on_page=10,
+                 delay_between_recipients=120):
+
+        self.username = username
+        self.password = password
+        self.message = message
+        self.proxy = proxy
+        self.delay_on_page = delay_on_page
+        self.delay_between_recipients = delay_between_recipients
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--disable-notifications')
+        # options.add_argument("--headless")
+        if self.proxy:
+            options.add_argument("--proxy-server={}".format(proxy))
+
+        print("+++++++++")
+        print(os.name)
+
+
+        # application_path = ""
+        # if getattr(sys, 'frozen', False):
+        #     application_path = os.path.dirname(sys.executable)
+        #     os.chdir(application_path)
+
+        print('CWD: ' + os.getcwd())
+        # print(application_path)
+
+        if os.name=='nt':
+            self.browser = webdriver.Chrome(chrome_options=options)
+        else:
+            current_dir = os.path.dirname(sys.executable)
+            chromedriver = os.path.join(current_dir, DRIVER_PATH)
+            print(chromedriver)
+            self.browser = webdriver.Chrome(chrome_options=options, executable_path=chromedriver)
+
+
+        self.browser.get('https://www.facebook.com')
+        email_elem = self.browser.find_element_by_id('email')
+        email_elem.send_keys(self.username)
+        pass_elem = self.browser.find_element_by_id('pass')
+        pass_elem.send_keys(self.password)
+        button_elem = self.browser.find_element_by_xpath("//input[@type='submit']")
+        button_elem.click()
+        time.sleep(2)
+
+
+    def get_message_url(self, recipient):
+        print("I am here")
+        path = recipient.split('/')[-1]
+        profile_key = 'profile.php?id='
+        if profile_key in path:
+            result = path.split(profile_key)[1].split('&')[0].strip()
+        else:
+            result = path.split('?')[0].strip()
+        return 'https://www.facebook.com/messages/t/%s' % result
+
+
+    def get_first_name(self, html, url):
+        key = '</span></h2>'
+        if key in html:
+            data = html.split(key)[0].split('">')[-1]
+            return data.split(' ')[0]
+        else:
+            print('No key "%s" in url "%s"' % (key,url))
+            return ''
+
+
+    def send(self, url):
+        print("Sending!")
+        self.browser.get(url)
+        time.sleep(self.delay_on_page)
+        if '<time class="' not in self.browser.page_source:
+            first_name = self.get_first_name(self.browser.page_source, url)
+            text = self.message.format(first_name=first_name)
+            text_elem = self.browser.switch_to.active_element
+            lines = text.split('\n')
+            for row in lines:
+                text_elem.send_keys(row)
+                ActionChains(self.browser).key_down(Keys.SHIFT).send_keys(Keys.RETURN).key_up(Keys.SHIFT).perform()
+            text_elem.send_keys(Keys.RETURN)
+        else:
+            print("Presents!")
+
+
+    def close(self):
+        print("Closing!")
+        self.browser.close()
+
+
+
+class Collector():
+    def __init__(self, username, password, url, proxy=None, loading_delay=10):
+
+        self.username = username
+        self.password = password
+        self.url = url
+        self.proxy = proxy
+        self.loading_delay = loading_delay
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--disable-notifications')
+        options.add_argument("--headless")
+        if self.proxy:
+            options.add_argument("--proxy-server={}".format(proxy))
+
+        if os.name=='nt':
+            self.browser = webdriver.Chrome(chrome_options=options)
+        else:
+            current_dir = os.path.dirname(sys.executable)
+            chromedriver = os.path.join(current_dir, DRIVER_PATH)
+            print(chromedriver)
+            self.browser = webdriver.Chrome(chrome_options=options,
+                                            executable_path=chromedriver)
+
+
+        self.browser.get('https://www.facebook.com')
+        email_elem = self.browser.find_element_by_id('email')
+        email_elem.send_keys(self.username)
+        pass_elem = self.browser.find_element_by_id('pass')
+        pass_elem.send_keys(self.password)
+        button_elem = self.browser.find_element_by_xpath("//input[@type='submit']")
+        button_elem.click()
+
+
+    def filter_users(self, raw_users):
+        result = []
+        for raw in raw_users:
+            profile = raw[0].split('?')[0].strip()
+            url_list = [href[0] for href in result]
+            if profile in url_list:
+                index = url_list.index(profile)
+                if len(result[index][1]) == 0:
+                    result[index][1] = raw[1]
+            else:
+                if self.url in profile:
+                    continue
+                result.append([profile, raw[1]])
+
+
+            # if profile not in [href[0] for href in result]:
+            # if profile not in result:
+                # result.append([profile, raw[1]])
+        return result
+
+
+    def get_commentors(self):
+        self.browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
+        time.sleep(self.loading_delay)
+        time.sleep(5)
+        try:
+            comments_block = self.browser.find_element_by_xpath("//div[@class='UFIList']")
+            view_more_key = '<a class="UFIPagerLink" href="#" role="button">View more comments</a>'
+            while view_more_key in comments_block.get_attribute('innerHTML'):
+                comments_block.find_element_by_xpath(".//a[@class='UFIPagerLink']").click()
+                time.sleep(6)
+            [elem.click() for elem in comments_block.find_elements_by_xpath(".//a[@class='UFIPagerLink']")]
+            time.sleep(self.loading_delay)
+            raw_commentors = [[e.get_attribute('href'), e.text] for e in comments_block.find_elements_by_xpath(".//a[@class=' UFICommentActorName']")]
+            commentors = self.filter_users(raw_commentors)
+        except Exception as err:
+            print(err)
+            self.close()
+        return commentors
+
+
+    def get_likers(self):
+        self.browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
+        time.sleep(self.loading_delay)
+        comments_block = self.browser.find_element_by_xpath("//div[@class='UFIList']")
+        comments_block.find_element_by_xpath(".//a[@rel='ignore']").click()
+        time.sleep(self.loading_delay)
+        likers_block = self.browser.find_element_by_xpath("//div[@style='opacity: 1;']")
+        more_key = 'See More'
+        while more_key in likers_block.get_attribute('innerHTML'):
+            likers_block.find_element_by_xpath(".//a[text()='See More']").click()
+            time.sleep(3)
+        raw_likers = [[e.get_attribute('href'), e.text] for e in likers_block.find_elements_by_xpath(".//a")]
+        likers = self.filter_users(raw_likers)
+        return likers
+
+    def collect(self):
+        self.browser.get(self.url)
+        commenters = self.get_commentors()
+        # likers = self.get_likers()
+
+        # collected = commenters + likers
+        # return collected
+        return commenters
+
+    def close(self):
+        print("Closing!")
+        self.browser.close()
+
+
+if __name__ == '__main__':
+    collector = Collector("georgegaucho@mail.com",
+                        "g30rg3g20ch0",
+                          "https://www.facebook.com/grantcardonefan/posts/10156060294808563")
+
+    data = collector.collect()
+    print(data)
+    collector.close()
---
+++ client_mac_os/main.py
@@ -0,0 +1,265 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+import sys
+import os
+import requests
+import time
+from fbtool import Messenger, Collector
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import QSize, QThreadPool, QRunnable, pyqtSlot, QTimer
+
+# our api url.
+API_URL = "https://outboundmessenger.com/api/"
+
+
+def get_token():
+    """
+    Get token for authentication.
+    """
+
+    current_dir = os.path.dirname(sys.executable)
+    # print("Token Path -> " + current_dir)
+    token_file = os.path.join(current_dir, "token.key")
+    if os.path.exists(token_file):
+        with open(token_file, "r") as f:
+            token = f.readline().strip()
+        return token
+
+
+def get_facebook_account():
+    """
+    Getting facebook account data.
+    """
+    headers = {"Authorization": "Token " + get_token()}
+    api_url = API_URL + "fbaccount/"
+    r = requests.get(api_url, headers=headers)
+    return r.json()[0]
+
+
+
+def update_profile_url(user_pk, task_id, done=False):
+    """
+    Updating status.
+    """
+    headers = {"Authorization": "Token " + get_token()}
+    api_url = API_URL + "fburls/{}/".format(user_pk)
+    data = {"pk": user_pk, "is_messaged": True,
+            "task_id": task_id, "done": done}
+    r = requests.post(api_url, data=data, headers=headers)
+    print(r)
+
+
+def create_profile_url(task_id, url, full_name, tag, done=False):
+    """
+    Create facebok url profile.
+    """
+    headers = {"Authorization": "Token " + get_token()}
+    api_url = API_URL + "fburlcreate/"
+    data = {"task_id": task_id,
+            "url": url,
+            "tag": tag,
+            "full_name": full_name,
+            "done": done}
+    print(data)
+    r = requests.post(api_url, data=data, headers=headers)
+    print(r.content)
+
+
+def empty_request(task_id, done=True):
+    """
+    Create facebok url profile.
+    """
+    headers = {"Authorization": "Token " + get_token()}
+    api_url = API_URL + "empty/"
+    data = {"task_id": task_id, "done": done}
+    r = requests.post(api_url, data=data, headers=headers)
+
+
+class MessengerWorker(QRunnable):
+    """
+    Message sending worker thread
+    """
+
+    def __init__(self, task_id, message, *args, **kwargs):
+        super(MessengerWorker, self).__init__()
+
+        self.task_id = task_id
+        self.message = message
+        self.args = args
+        self.kwargs = kwargs
+
+
+    @pyqtSlot()
+    def run(self):
+        """
+        Running main code in this thread.
+        Calling functions.
+        """
+
+        print("I am in Sender worker.")
+
+        headers = {"Authorization": "Token " + get_token()}
+        api_url = API_URL + "fburls/?task_id={}".format(self.task_id)
+        recipeints = requests.get(api_url, headers=headers)
+
+        print("Task ID", self.task_id)
+
+        username = get_facebook_account()["fb_user"]
+        password = get_facebook_account()["fb_pass"]
+        print(username, password)
+
+
+        messenger = Messenger(username, password, self.message)
+
+        for recipient in recipeints.json():
+            print(recipient)
+            message_url = messenger.get_message_url(recipient["url"])
+            messenger.send(message_url)
+            update_profile_url(recipient["pk"], self.task_id)
+            time.sleep(5)
+
+        update_profile_url(recipient["pk"], self.task_id, done=True)
+        messenger.close()
+
+
+class CollectorWorker(QRunnable):
+    """
+    Facbook profiles collector worker thread.
+    """
+
+    def __init__(self, task_id, url, tag, *args, **kwargs):
+        super(CollectorWorker, self).__init__()
+
+        self.task_id = task_id
+        self.url = url
+        self.tag = tag
+        self.args = args
+        self.kwargs = kwargs
+
+
+    @pyqtSlot()
+    def run(self):
+        """
+        Running main code in this thread.
+        Calling functions.
+        """
+
+        print("I am in Collector worker.")
+
+        username = get_facebook_account()["fb_user"]
+        password = get_facebook_account()["fb_pass"]
+
+        collector = Collector(username, password, self.url)
+
+        data = collector.collect()
+        print(data)
+
+        collector.close()
+
+        if data:
+            data_len = len(data) - 1
+            for key, profile in enumerate(data):
+                if key == data_len:
+                    done = True
+                else:
+                    done = False
+                create_profile_url(self.task_id,
+                                   profile[0],  profile[1], self.tag, done)
+        else:
+            empty_request(self.task_id)
+
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.threadpool = QThreadPool()
+
+        self.task_history = []
+
+        # self.token_file = os.path.join(sys.path[0], "token.key")
+        current_dir = os.path.dirname(sys.executable)
+        print("Token1 Path -> " + current_dir)
+        self.token_file = os.path.join(current_dir, "token.key")
+        self.setMinimumSize(QSize(320, 140))
+        self.setWindowTitle("Client Outboundmessenger.com")
+
+
+        self.nameLabel = QLabel(self)
+        self.nameLabel.setText('Token:')
+
+        self.token_line = QLineEdit(self)
+        self.token_line.setText(get_token())
+
+        self.infoLabel = QLabel(self)
+        self.infoLabel.setText('Keep it open!')
+
+        self.token_line.move(80, 20)
+        self.token_line.resize(200, 32)
+        self.nameLabel.move(20, 20)
+        self.infoLabel.move(20, 90)
+
+        save_button = QPushButton('Save', self)
+        save_button.clicked.connect(self.clickMethod)
+        save_button.resize(200,32)
+        save_button.move(80, 60)
+
+        self.token_line.returnPressed.connect(save_button.click)
+
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.check_task)
+        self.timer.start()
+
+
+    def check_task(self):
+        """
+        Check for new tasks.
+        Calling workers depending
+        on task type.
+        """
+        if get_token():
+            headers = {"Authorization": "Token " + get_token()}
+            r = requests.get(API_URL + "taskstatus/", headers=headers)
+
+            for data in r.json():
+                try:
+                    a = data["task_id"]
+                except:
+                    continue
+
+                if data["task_id"] not in self.task_history:
+                    if data["task_type"] == "m":
+                        print(data["task_id"])
+                        messenger_worker = MessengerWorker(data["task_id"],
+                                                        data["message"])
+                        self.threadpool.start(messenger_worker)
+
+                    if data["task_type"] == "c":
+                        collector_worker = CollectorWorker(data["task_id"],
+                                                        data["url"],
+                                                        data["tag"])
+                        self.threadpool.start(collector_worker)
+
+                    self.task_history.append(data["task_id"])
+
+
+    def clickMethod(self):
+        print('Your Token: ' + self.token_line.text())
+        print(self.token_file, "Saving... from button")
+        with open(self.token_file, "w") as f:
+            f.write(self.token_line.text())
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
